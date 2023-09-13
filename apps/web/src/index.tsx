b3d4aabaa5e1c0ca2aeb7dd416dbe86c0edf640a
tangtaoit <tangtaoit@githubim.com>
2023-09-13 20:47:57 +0800

    fix: support node 18

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -8,14 +8,20 @@ import  { LoginModule } from '@tsdaodao/login';
 import  { DataSourceModule } from '@tsdaodao/datasource';
 import {ContactsModule} from '@tsdaodao/contacts';
 
+const apiURL = "https://api.botgate.cn/v1/"
 
-if(process.env.NODE_ENV === "development") {
-  WKApp.apiClient.config.apiURL = "https://api.botgate.cn/v1/"
+if(!(window as any).__TAURI_IPC__) { // tauri环境
+  if(process.env.NODE_ENV === "development") {
+    WKApp.apiClient.config.apiURL = apiURL
+  }else {
+    WKApp.apiClient.config.apiURL = "/api/v1/" // 正式环境地址 (通用打包镜像，用此相对地址),打包出来的镜像可以通过API_URL环境变量来修改API地址
+  }
 }else {
-  WKApp.apiClient.config.apiURL = "/api/v1/" // 正式环境地址 (通用打包镜像，用此相对地址),打包出来的镜像可以通过API_URL环境变量来修改API地址
+  console.log("tauri环境")
+  WKApp.apiClient.config.apiURL = apiURL
 }
 
-WKApp.apiClient.config.apiURL = "https://api.botgate.cn/v1/"
+
 
 
 WKApp.apiClient.config.tokenCallback = ()=> {
